268afb83504ff11b4349e59adf069adfe2159de3
proses-p <prosesprojestus0@gmail.com>
2025-11-04 22:15:22 +0300

    Fix root URL to point to register page

--- registrationhub/urls.py
+++ registrationhub/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('register/', include('users.urls')),
+    path('', RedirectView.as_view(url='/register/', permanent=True)),
 ]
